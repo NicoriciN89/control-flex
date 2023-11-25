c7cf0c55a22857d4b96dce5b198ff7809472e262
NicoriciN89 <73961734+NicoriciN89@users.noreply.github.com>
2023-11-25 18:30:00 +0200

    Update food-dict_1.py

--- food-dict_1.py
+++ food-dict_1.py
@@ -7,25 +7,26 @@ order = {
 
 order['total'] = order['price'] * order['quantity']
 
-# Aplicați o reducere de 20% dacă cantitatea este mai mare de 7
+# Aplică o reducere de 20% dacă cantitatea este mai mare de 7
 if order['quantity'] > 7:
     order['discount'] = 0.2
     order['total'] *= (1 - order['discount'])
+
+# Întreabă utilizatorul dacă are nevoie de livrare
+delivery_input = input("Do you need delivery? (yes/no): ")
+
+if delivery_input == 'yes':
+    order['delivery'] = 50.00
+    order['total'] += order['delivery']
 
 print("ORDER FOR :", order['client'])
 print("FOOD      :", order['item'])
 print("pric x qty:", order['price'], 'x', order['quantity'])
 print("total     :", order['total'])
 
-# Întrebați utilizatorul dacă are nevoie de livrare
-delivery_answer = input("Do you need delivery? (yes/no): ")
+if 'discount' in order:
+    print("discount  :", order['discount'] * 100, '%')
 
-# Verificați dacă totalul este mai mare de 300 și utilizatorul dorește livrarea
-if order['total'] > 300 and delivery_answer == 'yes':
-    order['delivery'] = 'free'
-    print("Delivery   :", order['delivery'])
-else:
-    order['delivery'] = 'not free'
-
-print("Final Total:", order['total'])
+if 'delivery' in order:
+    print("delivery  :", order['delivery'])
 
